3074c2d788720177b87aa5d91511a09e02bbef76
Alan Storm <astorm@newrelic.com>
2019-12-07 14:26:32 -0800

    Now spans

--- src/client/spans/batch.ts
+++ src/client/spans/batch.ts
@@ -9,14 +9,36 @@ interface SpanBatchPayload {
 }
 
 export class SpanBatch implements SpanBatchPayload {
+  protected readonly LIMIT = 2000
   public spans: Span[]
 
   public constructor(spans: Span[] = []) {
     this.spans = spans
+
+    // if the client programmer passed us an array that's
+    // too big, keep the first `this.LIMIT` items and
+    // then use addSpan to add the rest (making the later
+    // items subject to the adaptive sampling)
+    if(this.spans.length > this.LIMIT) {
+      const remnant = this.spans.splice(this.LIMIT)
+      this.addSpan(...remnant)
+    }
   }
 
   public addSpan(...spans: Span[]): SpanBatch {
     this.spans.push(...spans)
+
+    // keep spans array at its limited value
+    while(this.LIMIT < this.spans.length) {
+      this.spans.splice(this.getRandomInt(0, this.LIMIT-1), 1)
+    }
     return this
+  }
+
+  // get a random number between min and max, inclusive
+  protected getRandomInt(min:number, max:number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * ((max + 1) - min)) + min; //The maximum is exclusive and the minimum is inclusive
   }
 }
--- tests/unit/limits.tap.ts
+++ tests/unit/limits.tap.ts
@@ -4,6 +4,11 @@ import {
   MetricBatch,
 } from '../../src/client/metrics'
 
+import {
+  Span,
+  SpanBatch,
+} from '../../src/client/spans'
+
 class MockMetricBatch extends MetricBatch {
   public mockGetMetrics() {
     return this.metrics
@@ -12,7 +17,16 @@ class MockMetricBatch extends MetricBatch {
   public mockGetLimit() {
     return this.LIMIT
   }
+}
 
+class MockSpanBatch extends SpanBatch {
+  public mockGetSpans() {
+    return this.spans
+  }
+
+  public mockGetLimit() {
+    return this.LIMIT
+  }
 }
 
 test('test limits on batches', (t) => {
@@ -42,7 +56,34 @@ test('test limits on batches', (t) => {
   })
 
   t.test('test span limits', (t) => {
-    t.ok(true, 'test setup')
+    const batch = new MockSpanBatch;
+    t.ok(0 === batch.mockGetSpans().length, 'new batch is empty')
+
+    // get a big'old array of metrics for us to reuse
+    let hugeSpans: Span[] = []
+    while(hugeSpans.length < batch.mockGetLimit() + 10) {
+      hugeSpans.push({
+        guid: Date.now().toString(), // TODO: Generate real GUID
+        name: 'limitTest',
+        entityName: 'test-entity',
+        traceId: 'abc123',
+        timestamp: Date.now(),
+        durationMs: 10,
+      })
+    }
+
+    //populate our first batch with too many spans
+    for(const idSpans of hugeSpans.entries()) {
+      batch.addSpan(idSpans[1])
+    }
+
+    t.ok(batch.mockGetLimit() === batch.mockGetSpans().length, 'batch did not collect too many spans')
+
+    // try creating a new batch with too many metrics
+    const batch2 = new MockSpanBatch(hugeSpans);
+    t.ok(batch2.mockGetLimit() === batch2.mockGetSpans().length, 'batch did not keep too large span array')
+    console.log("LIMIT: " + batch2.mockGetLimit())
+    console.log("LENGTH " + batch2.mockGetSpans().length)
     t.end()
   })
 
