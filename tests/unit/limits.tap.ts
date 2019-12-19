c81df67b276901d71be5c9394d1951b091ab07d0
Michael Goin <mgoin@newrelic.com>
2019-12-19 15:16:13 -0800

    Moves LIMIT off of batches which currently need to serialize cleanly for send to server.

--- tests/unit/limits.tap.ts
+++ tests/unit/limits.tap.ts
@@ -3,8 +3,7 @@ import uuidv4 from 'uuid/v4'
 
 import {
   SummaryMetric,
-  MetricBatch,
-  Metric
+  MetricBatch
 } from '../../src/client/metrics'
 
 import {
@@ -12,34 +11,17 @@ import {
   SpanBatch,
 } from '../../src/client/spans'
 
-class MockMetricBatch extends MetricBatch {
-  public mockGetMetrics(): Metric[] {
-    return this.metrics
-  }
-
-  public mockGetLimit(): number {
-    return this.LIMIT
-  }
-}
-
-class MockSpanBatch extends SpanBatch {
-  public mockGetSpans(): Span[] {
-    return this.spans
-  }
-
-  public mockGetLimit(): number {
-    return this.LIMIT
-  }
-}
+import {LIMIT as METRIC_LIMIT} from '../../src/client/metrics/batch'
+import {LIMIT as SPAN_LIMIT} from '../../src/client/spans/batch'
 
 test('test limits on batches', (t): void => {
   t.test('test metric limits', (t): void => {
-    const batch = new MockMetricBatch
-    t.ok(0 === batch.mockGetMetrics().length, 'new batch is empty')
+    const batch = new MetricBatch()
+    t.equal(batch.metrics.length, 0, 'new batch is empty')
 
     // get a big'old array of metrics for us to reuse
     let hugeMetrics: SummaryMetric[] = []
-    while (hugeMetrics.length < batch.mockGetLimit() + 10) {
+    while (hugeMetrics.length < METRIC_LIMIT + 10) {
       hugeMetrics.push(new SummaryMetric('foo'))
     }
 
@@ -48,27 +30,27 @@ test('test limits on batches', (t): void => {
       batch.addMetric(idMetrics[1])
     }
 
-    t.ok(
-      batch.mockGetLimit() === batch.mockGetMetrics().length,
-      'batch did not collect too many metrics'
-    )
+    t.equal(batch.metrics.length, METRIC_LIMIT, 'batch did not collect too many metrics')
 
     // try creating a new batch with too many metrics
-    const batch2 = new MockMetricBatch({ test: true }, Date.now(), 1000, hugeMetrics)
-    t.ok(
-      batch2.mockGetLimit() === batch2.mockGetMetrics().length,
+    const batch2 = new MetricBatch({ test: true }, Date.now(), 1000, hugeMetrics)
+
+    t.equal(
+      batch2.metrics.length,
+      METRIC_LIMIT,
       'batch did not keep too large metrics array'
     )
+
     t.end()
   })
 
   t.test('test span limits', (t): void => {
-    const batch = new MockSpanBatch
-    t.ok(0 === batch.mockGetSpans().length, 'new batch is empty')
+    const batch = new SpanBatch()
+    t.equal(batch.spans.length, 0, 'new batch is empty')
 
     // get a big'old array of metrics for us to reuse
     let hugeSpans: Span[] = []
-    while (hugeSpans.length < batch.mockGetLimit() + 10) {
+    while (hugeSpans.length < SPAN_LIMIT + 10) {
       hugeSpans.push({
         'id': uuidv4(),
         'trace.id': 'abc123',
@@ -86,17 +68,12 @@ test('test limits on batches', (t): void => {
       batch.addSpan(idSpans[1])
     }
 
-    t.ok(
-      batch.mockGetLimit() === batch.mockGetSpans().length,
-      'batch did not collect too many spans'
-    )
+    t.equal(batch.spans.length, SPAN_LIMIT, 'batch did not collect too many spans')
 
     // try creating a new batch with too many metrics
-    const batch2 = new MockSpanBatch({}, hugeSpans)
-    t.ok(
-      batch2.mockGetLimit() === batch2.mockGetSpans().length,
-      'batch did not keep too large span array'
-    )
+    const batch2 = new SpanBatch({}, hugeSpans)
+
+    t.equal(batch2.spans.length, SPAN_LIMIT, 'batch did not keep too large span array')
     t.end()
   })
 
