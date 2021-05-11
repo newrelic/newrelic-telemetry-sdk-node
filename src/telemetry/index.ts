92ee891d7f6fdac0361481b98be22f5b579e7971
mrickard <maurice@mauricerickard.com>
2021-05-11 18:16:17 -0400

    fix: Export logs interface
    Signed-off-by: mrickard <maurice@mauricerickard.com>

--- src/telemetry/index.ts
+++ src/telemetry/index.ts
@@ -1,8 +1,9 @@
 import * as events from './events'
+import * as logs from './logs'
 import * as metrics from './metrics'
 import * as spans from './spans'
 
-export { events, metrics, spans }
+export { events, logs, metrics, spans }
 
 export { RequestResponseError } from './base-client'
 export * from './response-parser'
